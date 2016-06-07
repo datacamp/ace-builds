4098607911df12e1fc6c50754f50be73b6a0788b
rduque1 <robin@datacamp.com>
2016-06-07 14:05:50 +0200

    set r consolemode

---
+++ src/snippets/r_console.js
@@ -0,0 +1,7 @@
+define("ace/snippets/r_console",["require","exports","module"], function(require, exports, module) {
+"use strict";
+
+exports.snippetText =undefined;
+exports.scope = "r_console";
+
+});
