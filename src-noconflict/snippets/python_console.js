32cf3f2f433587169726fc9eaaf632b286bbf211
Ludovic Vannoorenberghe <ludo@datacamp.com>
2017-05-23 13:51:07 +0200

    build v1.2.6-dc

---
+++ src-noconflict/snippets/python_console.js
@@ -0,0 +1,7 @@
+ace.define("ace/snippets/python_console",["require","exports","module"], function(require, exports, module) {
+"use strict";
+
+exports.snippetText =undefined;
+exports.scope = "python_console";
+
+});
---
+++ src/snippets/python_console.js
@@ -0,0 +1,7 @@
+define("ace/snippets/python_console",["require","exports","module"], function(require, exports, module) {
+"use strict";
+
+exports.snippetText =undefined;
+exports.scope = "python_console";
+
+});
